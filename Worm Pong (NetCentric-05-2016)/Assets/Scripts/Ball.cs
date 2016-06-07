950546cf01a846271137bb465cc3ec242eec6e4f
Daniel <ironygames@gmail.com>
2016-06-07 20:10:01 +0200

    - Added PlayerCharge - Added PlayerMovement - Added Variables for the Axis

---
+++ Worm Pong (NetCentric-05-2016)/Assets/Scripts/Ball.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+	private Vector2 direction;
+	public float speed;
+	public float bounceSpeedIncrement;
+	public float maxSpeed;
+	public int scoreToWin;
+
+	public ScoreBoard left, right;
+
+	private bool DEBUG = false;
+
+	void Start ()
+	{
+		resetBall ();
+	}
+
+
+	void Update ()
+	{
+		if (this.DEBUG == true) {
+			print (", Speed: " + direction.ToString ());
+		
+		}
+	}
+
+	private void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.name.Equals ("leftWall")) {
+			if (this.DEBUG == true) {
+				print ("Score for right!");
+			}
+			addPointToRight ();
+			resetBall ();
+		} else if (other.gameObject.name.Equals ("rightWall")) {
+			if (this.DEBUG == true) {
+				print ("Score for left!");
+			}
+			addPointToLeft ();
+			resetBall ();
+		}
+	}
+
+	private void OnCollisionEnter2D (Collision2D coll)
+	{
+		if (coll.gameObject.name.Equals ("upWall") || coll.gameObject.name.Equals ("downWall")) {
+			if (this.DEBUG == true) {
+				print ("Hit superior/inferior wall!");
+			}
+
+			direction.y = calculateBounceIncrementAndDirectionChange (direction.y, bounceSpeedIncrement);
+			direction.y = evaluateMaxDirectionSpeed (direction.y, maxSpeed);
+
+		} else if (coll.gameObject.tag.Equals ("paddle")) {
+			if (this.DEBUG == true) {
+				print ("Hit worm!");
+			}
+
+			direction.x = calculateBounceIncrementAndDirectionChange (direction.x, bounceSpeedIncrement);
+			direction.x = evaluateMaxDirectionSpeed (direction.x, maxSpeed);
+
+			//determine ball angle
+			float paddlePosition = coll.transform.position.y;
+			float paddleHeight = coll.transform.lossyScale.y;
+			float ballPosition = transform.position.y;
+
+			float paddlePart = (ballPosition - paddlePosition) / paddleHeight;
+
+			direction.y = paddlePart * Mathf.Abs (direction.x) * 10;
+		}
+	}
+
+	private float calculateBounceIncrementAndDirectionChange (float direction, float increment)
+	{
+		direction = -direction;
+		if (direction < 0) {
+			direction -= increment;
+		} else {
+			direction += increment;
+		}
+		return direction;
+	}
+
+	private float evaluateMaxDirectionSpeed (float direction, float max)
+	{
+		if (direction > max) {
+			direction = max;
+		}
+		return direction;
+	}
+
+	private void resetBall ()
+	{
+		GetComponent<Rigidbody2D> ().transform.position = new Vector2 (0, 0);
+		direction = new Vector2 (-speed, 0);
+	}
+
+	private void addPointToLeft ()
+	{
+		left.addScore ();
+	}
+
+	private void addPointToRight ()
+	{
+		right.addScore ();
+	}
+
+	void FixedUpdate ()
+	{
+		GetComponent<Rigidbody2D> ().velocity = direction;
+	}
+		
+}
---
+++ Worm Pong (NetCentric-05-2016)/Assets/Scripts/PlayerScripts/Player.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	private bool isUp, isDown, isCharging;
+	private float speed, acceleration;
+	public float accelerationIncrement, friction, maxSpeed, maxAcceleration, minSpeedThreshold;
+	public KeyCode upKey, downKey;
+	public string inputAxis;
+	public string inputChargeButton;
+
+
+	// Use this for initialization
+
+	private bool DEBUG = false;
+
+	void Start ()
+	{
+		isUp = false;
+		isDown = false;
+		speed = 0;
+		if (friction > 1) { //it's a percentage!
+			friction = 0.3f;
+		}
+	}
+
+	private void evauluateInput ()
+	{
+		float axis = Input.GetAxisRaw (inputAxis);
+		int charge = (int)Input.GetAxisRaw (inputChargeButton);
+
+		if (DEBUG == true) {
+			print ("Axis: " + axis);
+		}
+
+		isUp = false;
+		isDown = false;
+
+		if (axis == 1) {
+			isUp = true;
+		} else if (axis == -1) {
+			isDown = true;
+		}
+
+		if (charge == 0) {
+			isCharging = false;
+		} else {
+			isCharging = true;
+		}
+
+
+	}
+
+	private void calculateKinematics ()
+	{
+		//take care of input
+		if (isUp) {
+			acceleration = accelerationIncrement;
+		} else if (isDown) {
+			acceleration = -accelerationIncrement;
+		} else {
+			acceleration = 0;
+		}
+		if (acceleration > maxAcceleration) {
+			acceleration = maxAcceleration;
+		} else if (acceleration < -maxAcceleration) {
+			acceleration = -maxAcceleration;
+		}
+
+		//process into speed.
+		speed += (acceleration);
+		speed *= (1 - friction);
+
+		if (speed > maxSpeed) {
+			speed = maxSpeed;
+		} else if (speed < -maxSpeed) {
+			speed = -maxSpeed;
+		}
+
+		if (speed < minSpeedThreshold && speed > -minSpeedThreshold) {
+			speed = 0;
+		}
+
+		if (this.DEBUG == true) {
+			print ("Acceleration: " + acceleration + ", Speed: " + speed + ", Position: " + transform.position.y);
+		}
+	}
+
+	void Update ()
+	{
+		evauluateInput ();
+		calculateKinematics ();
+	}
+
+	void FixedUpdate ()
+	{
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, speed);
+	}
+
+}
